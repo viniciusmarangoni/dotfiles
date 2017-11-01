98e81c6969b7debb5f7b401ad63347a55e7a6568
Vinicius Henrique Marangoni <vinicius_marangoni1@hotmail.com>
2017-10-31 23:40:41 -0200

    add emacs.el

---
+++ .emacs.el
@@ -0,0 +1,60 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+
+(setq custom-file "~/.emacs.d/custom.el")    ; Avoid writing in this file (.emacs.el)
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(setq myPackages
+      '(doom-themes
+        rainbow-delimiters
+        powerline
+        autopair))
+
+;; Install myPackages if not installed
+(dolist (package myPackages)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Theme
+(require 'doom-themes)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+(load-theme 'doom-one t)
+(doom-themes-visual-bell-config)
+(doom-themes-org-config)
+
+;; Custom configs
+(setq-default c-basic-offset 4)                ; Use tab-width for C offset
+(global-linum-mode 1)                          ; Show line numbers on buffers
+(setq inhibit-startup-message t)               ; No message at startup
+(setq-default indent-tabs-mode nil)            ; Use spaces instead of tabs
+(setq-default tab-width 4)                     ; Length of tab is 4 spaces
+(setq blink-matching-paren-distance nil)       ; Blinking parenthesis
+(setq echo-keystrokes 0.1)                     ; Echo Keystroke Delay
+(setq visible-bell t)                          ; Removes audible warnings
+(setq line-number-mode t)                      ; Display current line
+(global-hl-line-mode 1)                        ; highlight current line
+(tool-bar-mode -1)                             ; Disable toolbar
+(scroll-bar-mode -1)                           ; Disable scrollbar
+(menu-bar-mode -1)                             ; Disable menubar
+(show-paren-mode 1)                            ; Highlight parenthesis pairs
+(set-face-background 'show-paren-match-face "#ffffff") ; Color of parenthesis pairs
+(set-face-attribute 'show-paren-match-face nil         ;;
+                    :weight 'bold :underline nil :overline nil :slant 'normal)
+
+(setq-default show-paren-style 'mixed)         ; Set highlight (exp/mixed/paren)
+(fset 'yes-or-no-p 'y-or-n-p)                  ; y/n instead of yes/no
+
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook                      ; Programming modes only
+  'rainbow-delimiters-mode)
+
+(require 'powerline)
+(powerline-center-theme)
+
+(require 'autopair)                            ; Automagically pair braces and quotes
+(autopair-global-mode)                         ; to enable in all buffers
